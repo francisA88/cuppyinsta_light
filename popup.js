6aa98b71fa2038d60e7704ed558980a9dfd43fac
Francis Ali <githubemail61@gmail.com>
2021-01-22 21:59:14 +0100

    first commit

--- popup.js
+++
@@ -1,7 +0,0 @@
-const popup = document.querySelector(".popup-background")
-const trigger = document.querySelector("#popup-trigger")
-const cancelBtn = document.querySelector(".popup>.cancel")
-trigger.addEventListener("click", (e)=>{
-  popup.style.display = "flex";
-})
-cancelBtn.onclick = e=>popup.style.display = "none"
